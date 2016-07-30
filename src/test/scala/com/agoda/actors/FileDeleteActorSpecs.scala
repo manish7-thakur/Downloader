b2ed5a366a746accdacf96b0374c79b7c19fed98
Manish <thakur@zorgdomein.nl>
2016-07-30 15:05:43 +0530

    Added file delete actor

---
+++ src/test/scala/com/agoda/actors/FileDeleteActorSpecs.scala
@@ -0,0 +1,38 @@
+package com.agoda.actors
+
+import java.nio.file.{Files, Paths}
+
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestKit}
+import com.agoda.actors.DeleteFileFlow.{DeleteFile, FileDeleted}
+import com.typesafe.config.ConfigFactory
+import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
+
+class FileDeleteActorSpecs extends TestKit(ActorSystem("DeleteFileSpec", ConfigFactory.load("test"))) with ImplicitSender with WordSpecLike with BeforeAndAfterAll  {
+
+  override def afterAll() = TestKit.shutdownActorSystem(system)
+  "FileDeleteActor" should {
+    "delete the file if exists" in {
+      val pathString = "src/test/resources/victim"
+      Files.createFile(Paths.get(pathString))
+      val actor = system.actorOf(Props[FileDeleteActor], "FileDeleteActor")
+      actor ! DeleteFile(pathString)
+      expectMsg(FileDeleted)
+    }
+  }
+}
+
+class FileDeleteActor extends Actor {
+  def receive = {
+    case DeleteFile(path) => {
+      Files.deleteIfExists(Paths.get(path))
+      sender ! FileDeleted
+    }
+  }
+}
+
+object DeleteFileFlow{
+  case class DeleteFile(path: String)
+  case object FileDeleted
+}
+
