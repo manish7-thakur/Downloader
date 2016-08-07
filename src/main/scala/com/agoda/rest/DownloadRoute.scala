970e29062ed97ff06574e563ec6f0ddd5d8b1eb2
Manish <thakur@zorgdomein.nl>
2016-08-08 00:36:01 +0530

    Created route for bulk download

--- src/main/scala/com/agoda/rest/DownloadRoute.scala
+++ src/main/scala/com/agoda/rest/DownloadRoute.scala
@@ -2,6 +2,7 @@ package com.agoda.rest
 
 import akka.actor.ActorRef
 import com.agoda.actors.DownloadFile
+import com.agoda.actors.DownloadFlow.{BulkDownload, BulkDownloadMode}
 import com.agoda.dto.DownloadFileDto
 import com.agoda.dto.DownloadFileJsonProtocol._
 import spray.httpx.SprayJsonSupport._
@@ -11,10 +12,22 @@ trait DownloadRoute extends HttpService {
 
   def downloadFlow(context: RequestContext): ActorRef
 
-  def downloadRoute = pathPrefix("api" / "download") {
-    post {
-      entity(as[DownloadFileDto]) { dto =>
-        requestContext => downloadFlow(requestContext) ! DownloadFile(dto)
+  def downloadRoute = pathPrefix("api") {
+    path("download") {
+      post {
+        entity(as[DownloadFileDto]) { dto =>
+          requestContext => downloadFlow(requestContext) ! DownloadFile(dto)
+        }
+      }
+    } ~ path("bulkdownload") {
+      post {
+        entity(as[List[String]]) { urlList =>
+          requestContext => {
+            val flowActor = downloadFlow(requestContext)
+            flowActor ! BulkDownloadMode
+            flowActor ! BulkDownload(urlList, "defaultLocation")
+          }
+        }
       }
     }
   }
--- src/test/scala/com/agoda/rest/DownloadRouteSpecs.scala
+++ src/test/scala/com/agoda/rest/DownloadRouteSpecs.scala
@@ -3,6 +3,7 @@ package com.agoda.rest
 import akka.actor.{Actor, ActorRefFactory}
 import akka.testkit.TestActorRef
 import com.agoda.actors.DownloadFile
+import com.agoda.actors.DownloadFlow.{BulkDownload, BulkDownloadMode}
 import com.agoda.dto.DownloadFileDto
 import com.agoda.dto.DownloadFileJsonProtocol._
 import org.specs2.mutable.Specification
@@ -17,6 +18,8 @@ class DownloadRouteSpecs extends Specification with Specs2RouteTest with Downloa
   override def downloadFlow(requestContext: RequestContext) = TestActorRef(new Actor {
     def receive = {
       case DownloadFile(url, location) => requestContext.complete(DownloadFileDto(url, location))
+      case BulkDownloadMode =>
+      case BulkDownload(urlList, "defaultLocation") => requestContext.complete(urlList)
     }
   })
 
@@ -24,12 +27,22 @@ class DownloadRouteSpecs extends Specification with Specs2RouteTest with Downloa
     "POST on api/download" should {
       "unmarshall the Json to DownloadFile entity" in {
         Post("/api/download", DownloadFileDto("url", "location")) ~> downloadRoute ~> check {
-          responseAs[DownloadFileDto] === DownloadFileDto("url", "location")
+          responseAs[DownloadFileDto] shouldEqual DownloadFileDto("url", "location")
         }
       }
       "reject the request if invalid data is posted" in {
         Post("/api/download", "invalid data") ~> downloadRoute ~> check {
           handled must beFalse
+        }
+      }
+    }
+  }
+  "/buldownload" >> {
+    "POST on api/bulkdownload" should {
+      "unmarshall the json list to list of url strings" in {
+        Post("/api/bulkdownload", List("url1", "url2")) ~> downloadRoute ~> check {
+          handled must beTrue
+          responseAs[List[String]] shouldEqual List("url1", "url2")
         }
       }
     }
